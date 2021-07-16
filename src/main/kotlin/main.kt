ac4084836272a493a6a84e559bf95c0597ba7c0a
vlad <vlad.izidaev@gmail.com>
2021-07-16 15:02:05 +0500

    second

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,13 +3,12 @@ fun main(args: Array<String>) {
 }
 
 fun cult() {
-     val user = mapOf(Int to Int, Int to Int)
 
      println("Тип математической операции:")
-//     val opp = readLine()!!
+     val opp = readLine()!!
 
      println("Введите значение ")
-//     val list = listOf<Int>( readLine()!!.toInt(),  readLine()!!.toInt(),  readLine()!!.toInt())
+     val list = listOf<Int>( readLine()!!.toInt(),  readLine()!!.toInt(),  readLine()!!.toInt())
 
      println("Результат")
      //todo Установить git добавить проект ,сохранить то что есть на данный момент
@@ -19,21 +18,17 @@ fun cult() {
      // todo считывание матрицы из консоли 1) длина матрицы 2) ширина матрицы через консоль самому написать матрицу, строковые значения (вычесление суммы диагонали)должен уметь сумму диогоналей, вычислить сумму n-ряда, сумму горизонт, сумму вертикали, с указанием какой именно ты хочешь
     // todo использовать array(можно сразу указать размер элементов)
 
-//
-//     val res: Int = when (opp) {
-//          "+" -> list[0] + list[1]
-//          "-" -> list[0] - list[1]
-//          "/" -> list[0] / list[1]
-//          "*" -> list[0] * list[1]
-//          else -> 0
-//     }
-     user.forEach {key, value-> readLine()!!.toInt()}
+
+     val res: Int = when (opp) {
+          "+" -> list[0] + list[1]
+          "-" -> list[0] - list[1]
+          "/" -> list[0] / list[1]
+          "*" -> list[0] * list[1]
+          else -> 0
+     }
 
 
-     println(user.forEach())
 
-}
-
-private fun <K, V> Map<K, V>.forEach() {
+     println(res)
 
 }
